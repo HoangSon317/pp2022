7498c15b39027565e381bc8a252ab236fbe86c64
HoangSon317 <90445127+HoangSon317@users.noreply.github.com>
2022-04-10 13:25:17 +0700

    Create 2.LeHoangSon.BA10-051.py

---
+++ 2.LeHoangSon.BA10-051.py
@@ -0,0 +1,47 @@
+public class Student {
+    
+    private int ID;
+    private String Name;
+    private float mark1, mark2;
+ 
+    public Student() {
+    }
+ 
+    
+    public Student(int ID, String Name, Float mark1, Float mark2) {
+        this.ID = ID;
+        this.Name = Name;
+        this.mark2 = mark2;
+        this.mark1 = mark1;
+    }
+  public int getID() {
+        return ID;
+    }
+ 
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+ 
+    public String getName() {
+        return Name;
+    }
+ 
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+ 
+    public float getmark1() {
+        return mark1;
+    }
+ 
+    public void setmark2(float mark2) {
+        this.mark2 = mark2;
+    }
+ 
+    public float getmark2() {
+        return mark2;
+    }
+ 
+    public void setmark2(float mark2) {
+        this.mark2 = mark2;
+    }
