b4cdbbc3ecbf5d3e73cdd65e8634ba726fab2af2
HoangSon317 <90445127+HoangSon317@users.noreply.github.com>
2022-04-10 13:16:08 +0700

    Create 3.LeHoangSon.BA10-051.py

---
+++ 3.LeHoangSon.BA10-051.py
@@ -0,0 +1,26 @@
+>> round(10000 / 3, 2)
+
+3333.33
+>> round(10000 / 3, 1)
+
+3333.3
+>> round(10000 / 3, 0)
+
+3333.0
+>> round(10000 / 3, -1)
+
+3330.0
+>> round(10000 / 3, -2)
+
+3300.0
+Quang són
+f = 123.456
+
+print(type(f))
+# <class 'float'>
+
+print(round(f))
+# 123
+
+print(type(round(f)))
+# <class 'int'>
